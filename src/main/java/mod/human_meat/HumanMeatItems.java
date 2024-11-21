dd12844301f71c7e5bcc890fc5a9af7d2864847a
Domza64 <juricicdominik@protonmail.com>
2024-11-21 21:36:04 +0100

    Add Skinner Villager

--- src/main/java/mod/human_meat/HumanMeatItems.java
+++ src/main/java/mod/human_meat/HumanMeatItems.java
@@ -7,6 +7,7 @@ import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.village.VillagerProfession;
 
 public class HumanMeatItems {
     public static void initialize() {
@@ -28,7 +29,6 @@ public class HumanMeatItems {
     public static Item register(Item item, String id) {
         // Create the identifier for the item.
         Identifier itemID = Identifier.of(HumanMeat.MOD_ID, id);
-
         // Register the item.
         Item registeredItem = Registry.register(Registries.ITEM, itemID, item);
 
---
+++ src/main/java/mod/human_meat/datagen/HumanMeatPoiTagProvider.java
@@ -0,0 +1,24 @@
+package mod.human_meat.datagen;
+
+import mod.human_meat.HumanMeat;
+import net.minecraft.data.DataOutput;
+import net.minecraft.data.server.tag.TagProvider;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.PointOfInterestTypeTags;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.poi.PointOfInterestType;
+
+import java.util.concurrent.CompletableFuture;
+public class HumanMeatPoiTagProvider extends TagProvider<PointOfInterestType> {
+    public HumanMeatPoiTagProvider(DataOutput output,
+                             CompletableFuture<RegistryWrapper.WrapperLookup> registryLookupFuture) {
+        super(output, RegistryKeys.POINT_OF_INTEREST_TYPE, registryLookupFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup lookup) {
+        this.getOrCreateTagBuilder(PointOfInterestTypeTags.ACQUIRABLE_JOB_SITE)
+                .addOptional(new Identifier(HumanMeat.MOD_ID, "skinnerpoi"));
+    }
+}
